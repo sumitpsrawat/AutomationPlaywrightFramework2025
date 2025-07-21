e931d73fb16827c35f4444b849b5a12486fb6644
sumitpsrawat <139340955+sumitpsrawat@users.noreply.github.com>
2025-07-21 23:25:59 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,12 +24,12 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false, // Run tests in headless mode by default
+    headless: true, // Run tests in headless mode by default
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
     viewport: null, // Use default viewport size
